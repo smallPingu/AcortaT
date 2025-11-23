b83210e351069dfa0a1ec84aaccfab1843cb486c
smallPingu <arturomolina@correo.ugr.es>
2025-11-23 19:13:36 +0100

    feat: se implementó rate limiting(closes #3) cerramos issue correcta

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { UrlsModule } from './urls/urls.module';
 @Module({
   imports: [
     UrlsModule, 
-    
+    // 60 seg, 10 limite de peticiones
     ThrottlerModule.forRoot([{
       ttl: 60000,
       limit: 10,
