d5d6542fb8cd99939692c9d65d7a4152eea8a024
smallPingu <arturomolina@correo.ugr.es>
2025-11-23 12:18:40 +0100

    DTos añadidos para filtrar cadenas de texto que no sean URLs

--- src/urls/dto/create-url.dto.ts
+++ src/urls/dto/create-url.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUrlDto {}
+import { IsNotEmpty, IsUrl } from 'class-validator';
+
+export class CreateUrlDto {
+  @IsNotEmpty({ message: 'La URL no puede estar vacía' })
+  @IsUrl({}, { message: 'Debes proporcionar una URL válida (ej: https://google.com)' })
+  originalUrl: string;
+}
