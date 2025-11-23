a1aa5e6912e1649451306fcbb16c0cbdd3f20b1c
smallPingu <arturomolina@correo.ugr.es>
2025-11-23 13:39:31 +0100

    Añadimos la lógica de creación del Url acortado, que se incluye en la base de datos junto a la URL que pasó el usuario. Además se adapta el controller para hacer Post(), Get() y Get(:id) (todos o uno determinado)

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UrlsService } from './urls.service';
 import { CreateUrlDto } from './dto/create-url.dto';
-import { UpdateUrlDto } from './dto/update-url.dto';
 
 @Controller('urls')
 export class UrlsController {
   constructor(private readonly urlsService: UrlsService) {}
 
-  @Post()
+  @Post() 
   create(@Body() createUrlDto: CreateUrlDto) {
     return this.urlsService.create(createUrlDto);
   }
@@ -20,15 +19,5 @@ export class UrlsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.urlsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUrlDto: UpdateUrlDto) {
-    return this.urlsService.update(+id, updateUrlDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.urlsService.remove(+id);
   }
 }
--- src/urls/urls.service.ts
+++ src/urls/urls.service.ts
@@ -1,11 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUrlDto } from './dto/create-url.dto';
 import { UpdateUrlDto } from './dto/update-url.dto';
+import { PrismaService } from '../prisma.service';
+import { nanoid } from 'nanoid';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UrlsService {
-  create(createUrlDto: CreateUrlDto) {
-    return 'This action adds a new url';
+
+  constructor(private prisma: PrismaService) { }
+
+  async create(createUrlDto: CreateUrlDto) {
+    const { originalUrl } = createUrlDto;
+
+    let retries = 5;
+
+    while (retries > 0) {
+      const shortCode = nanoid(6);
+
+      try {
+        const newUrl = await this.prisma.url.create({
+          data: {
+            originalUrl,
+            shortCode,
+          },
+        });
+
+        return newUrl;
+
+      } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
+          console.log(`Colisión detectada con ${shortCode}, reintentando...`);
+          retries--;
+        } else {
+          throw new InternalServerErrorException();
+        }
+      }
+    }
+
+    throw new InternalServerErrorException('No se pudo generar un código único');
   }
 
   findAll() {
