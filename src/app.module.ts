e316c5d550722c7f330dd5f2f0d5c9bfb12d3911
smallPingu <arturomolina@correo.ugr.es>
2025-11-23 19:10:50 +0100

    feat: se implementó rate limiting (closes #4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UrlsModule } from './urls/urls.module';
 
 @Module({
-  imports: [UrlsModule],
+  imports: [
+    UrlsModule, 
+    
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
